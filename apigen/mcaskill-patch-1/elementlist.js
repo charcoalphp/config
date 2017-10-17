e983b5d6b1d9b04ea3c6e4fcd7e012d8f8395eb3
travis-ci <travis@travis-ci.org>
2017-10-17 01:45:49 +0000

    ApiGen (Travis Build : 58  - Branch : mcaskill-patch-1)

--- apigen/mcaskill-patch-1/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Charcoal\\Config\\AbstractConfig"],["c","Charcoal\\Config\\AbstractEntity"],["c","Charcoal\\Config\\ConfigInterface"],["c","Charcoal\\Config\\ConfigurableInterface"],["c","Charcoal\\Config\\ConfigurableTrait"],["c","Charcoal\\Config\\EntityInterface"],["c","Charcoal\\Config\\GenericConfig"]];
