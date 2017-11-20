120aee07da9eda270eacd537881c5b4269652615
travis-ci <travis@travis-ci.org>
2017-11-20 16:05:37 +0000

    ApiGen (Travis Build : 60  - Branch : 0.8)

---
+++ apigen/0.8/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Charcoal\\Config\\AbstractConfig"],["c","Charcoal\\Config\\AbstractEntity"],["c","Charcoal\\Config\\ConfigInterface"],["c","Charcoal\\Config\\ConfigurableInterface"],["c","Charcoal\\Config\\ConfigurableTrait"],["c","Charcoal\\Config\\DelegatesAwareInterface"],["c","Charcoal\\Config\\DelegatesAwareTrait"],["c","Charcoal\\Config\\EntityInterface"],["c","Charcoal\\Config\\GenericConfig"],["c","Charcoal\\Config\\SeparatorAwareInterface"],["c","Charcoal\\Config\\SeparatorAwareTrait"]];
